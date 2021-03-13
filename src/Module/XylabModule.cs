20609fe75700b3da08f1456a73597336e0facc67
小羊 <tlylz99@live.cn>
2021-03-13 23:48:41 +0800

    Initialize.

---
+++ src/Module/XylabModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SatelliteSite.XylabModule
+{
+    public class XylabModule : AbstractModule
+    {
+        public override string Area => "Xylab";
+
+        public override void Initialize()
+        {
+        }
+
+        public override void RegisterEndpoints(IEndpointBuilder endpoints)
+        {
+            endpoints.MapControllers();
+        }
+    }
+}
