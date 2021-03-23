854f366e64a68f6dd6bd2dc399922d23f57ee121
小羊 <tlylz99@live.cn>
2021-03-23 18:43:27 +0800

    Upgrade CCS to version 3.

--- src/Host/Migrations/20210323104216_NewRoleDefinitions.Designer.cs
+++ src/Host/Migrations/20210323104216_NewRoleDefinitions.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SatelliteSite;
 
 namespace SatelliteSite.Migrations
 {
     [DbContext(typeof(DefaultContext))]
-    partial class DefaultContextModelSnapshot : ModelSnapshot
+    [Migration("20210323104216_NewRoleDefinitions")]
+    partial class NewRoleDefinitions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Host/Migrations/20210323104216_NewRoleDefinitions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SatelliteSite.Migrations
+{
+    public partial class NewRoleDefinitions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName", "ShortName" },
+                values: new object[] { -30, "40100c40-6ca5-7bcb-48bc-41f2a939cbee", "CDS API user", "CDS", "CDS", "cds_api" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName", "ShortName" },
+                values: new object[] { -31, "8f8c37e1-a309-bd2d-6708-0519df89139b", "Contest Creator", "ContestCreator", "CONTESTCREATOR", "cont" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -31);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -30);
+        }
+    }
+}
