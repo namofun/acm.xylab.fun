e2e9f72d9abacf04a6afa05029bad01f0b314401
小羊 <tlylz99@live.cn>
2021-03-25 22:15:39 +0800

    Upgrade CCS to v4.

---
+++ src/Host/Migrations/20210325140907_BumpToCcs4.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SatelliteSite.Migrations
+{
+    public partial class BumpToCcs4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviousRating",
+                table: "ContestMembers");
+
+            migrationBuilder.DropColumn(
+                name: "RatingDelta",
+                table: "ContestMembers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName", "ShortName" },
+                values: new object[] { -32, "d68c9040-7762-ab0b-06eb-19ce1b5a5120", "Temporary Team Account", "TemporaryTeamAccount", "TEMPORARYTEAMACCOUNT", "temp_team" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -32);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PreviousRating",
+                table: "ContestMembers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RatingDelta",
+                table: "ContestMembers",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
