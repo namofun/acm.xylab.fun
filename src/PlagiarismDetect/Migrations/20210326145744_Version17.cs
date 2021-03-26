412c6e8da33c247a67db16d17776900bcfb4cc6d
小羊 <tlylz99@live.cn>
2021-03-26 23:07:54 +0800

    Upgrade dependency version.

---
+++ src/PlagiarismDetect/Migrations/20210326145744_Version17.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SatelliteSite.Migrations
+{
+    public partial class Version17 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Justification",
+                table: "PlagiarismReports",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Justification",
+                table: "PlagiarismReports");
+        }
+    }
+}
