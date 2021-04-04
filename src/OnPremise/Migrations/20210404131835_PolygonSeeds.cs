182545b6790885e5550d37b4864954900cab3e64
小羊 <tlylz99@live.cn>
2021-04-04 21:19:12 +0800

    Add polygon seeds for JOP.

---
+++ src/OnPremise/Migrations/20210404131835_PolygonSeeds.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SatelliteSite.Migrations
+{
+    public partial class PolygonSeeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            Polygon.Storages.SeedMigrationV1.Up(migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
