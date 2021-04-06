a77bb769776058a681126f184e5edf10b6962aed
小羊 <tlylz99@live.cn>
2021-04-06 20:29:43 +0800

    Add scoreboard exporting.

---
+++ src/Ccs.Connector.OpenXml/OpenXmlConnector.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[assembly: AffiliateTo(
+    typeof(Ccs.Connector.OpenXml.OpenXmlConnector),
+    typeof(SatelliteSite.ContestModule.ContestModule<>))]
+
+namespace Ccs.Connector.OpenXml
+{
+    public class OpenXmlConnector : AbstractConnector
+    {
+        public override string Area => "Contest";
+    }
+}
