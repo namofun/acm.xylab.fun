5af1ac7d20fa4efaea1a564e7c374abba9d5aac8
XiaoYang <tlylz99@live.cn>
2022-07-02 22:52:11 +0800

    Split for Windows and Linux

---
+++ src/BricksService.Linux/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(Xylab.BricksService.Startup))]
+
+namespace Xylab.BricksService
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<ITelemetryClient, FunctionsTelemetryClient>();
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+    }
+}
